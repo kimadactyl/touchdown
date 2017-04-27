dd9998f09e8b064e198ec59504b5705580f41a2d
Kim Foale <kim@alliscalm.net>
2017-04-27 11:09:41 +0100

    Add high contrast switcher

---
+++ docs/assets/styleswitcher.js
@@ -0,0 +1,45 @@
+$(function() {
+
+  // HTML IDs of our elements
+  var highContrast = $('#style-high')[0];
+  var normalContrast =  $('#style-main')[0];
+  var contrastToggle = $('#high-contrast');
+
+  // Track contrast state across pages
+  var contrastEnabled = localStorage.getItem('highContrast') || false;
+
+  // Functions to enable and disable appropriate stylesheets
+  function enableHigh() {
+    highContrast.disabled = false;
+    highContrast.rel = 'stylesheet';
+    normalContrast.disabled = true;
+    normalContrast.rel = 'stylesheet alternate';
+  };
+
+  function enableMain() {
+    normalContrast.disabled = false;
+    normalContrast.rel = 'stylesheet';
+    highContrast.rel = 'stylesheet alternate';
+    highContrast.disabled = true;
+  };
+
+  // Bind the events to the page toggle
+  contrastToggle.click(function() {
+    // JSON-ify it to clean up localStorage turning true into a string
+    contrastEnabled = !JSON.parse(contrastEnabled);
+    if(contrastEnabled) {
+      enableHigh();
+    } else {
+      enableMain();
+    }
+    localStorage.setItem('highContrast', contrastEnabled);
+    console.log(contrastEnabled);
+  });
+
+  // Check when the page is loaded if we need to enable high contrast
+  if(contrastEnabled == "true") {
+    console.log(contrastEnabled);
+    enableHigh();
+  }
+
+})
